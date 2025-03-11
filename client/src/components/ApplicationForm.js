46cec3ee23c38aab404b7a529e6f04195922c251
Diluth Rangana <138687673+diluthrangana@users.noreply.github.com>
2025-03-12 00:19:31 +0530

    Update ApplicationForm.js

--- client/src/components/ApplicationForm.js
+++ client/src/components/ApplicationForm.js
@@ -123,7 +123,7 @@ const ApplicationForm = () => {
               href="https://job-application-processing-pipeline-3.onrender.com/" 
               target="_blank" 
               rel="noopener noreferrer"
-              style={{ textDecoration: 'none', fontWeight: 'bold', color: '#00afee' }}
+              style={{ textDecoration: 'none', fontWeight: 'bold', color: '#00e6ee' }}
             > here </a>  
             to wake up the server and try again.
           </p>
